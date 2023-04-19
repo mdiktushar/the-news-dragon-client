79fcf05034be9b18b2709c1029a46820c1c3c89f
mdiktushar <md.tushar1804@gmail.com>
2023-04-19 16:00:44 +0600

    Module Overview, Project setup and router setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider } from 'react-router-dom';
+import Router from './routes/routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Router}></RouterProvider>
   </React.StrictMode>,
 )
